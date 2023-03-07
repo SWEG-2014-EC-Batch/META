04b3350f407aac7fc6a896a8f9ad12655125efd8
Leul-Alemayehu <124813172+Leul-Alemayehu@users.noreply.github.com>
2023-03-07 15:25:42 +0300

    Fixed some nonsense

--- TestFiles/FibonacciTrainwreck.cpp
+++ TestFiles/FibonacciTrainwreck.cpp
@@ -8,12 +8,22 @@ int main(){
     long double inputVar;
     std::cout << "Until whatth Fibonacci number do you want the sequence written out? ";
     std::cin >> inputVar;
-    std::cout << smallerVar << '\n' << biggerVar << '\n';
-    for(long double i = 1; i<=inputVar-2; i++){
-        dummyVar=biggerVar;
-        sumVar=smallerVar+biggerVar;
-        std::cout << std::setprecision (30) << sumVar << std::endl;
-        biggerVar = sumVar;
-        smallerVar = dummyVar;
+    if(inputVar==1){
+        std::cout << smallerVar;
+        return 0;
+    }
+    else if (inputVar==2){
+        std::cout << smallerVar << '\n' << biggerVar << std::endl;
+        return 0;
+    }
+    else{
+        std::cout << smallerVar << '\n' << biggerVar << '\n';
+        for(long double i = 1; i<=inputVar-2; i++){
+            dummyVar=biggerVar;
+            sumVar=smallerVar+biggerVar;
+            std::cout << std::setprecision (30) << sumVar << std::endl;
+            biggerVar = sumVar;
+            smallerVar = dummyVar;
+    }
     }
 }
