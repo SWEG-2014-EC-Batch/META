048dfd77342311651834cd086bf650fc4f262726
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2023-03-15 21:37:37 +0300

    print pattern

---
+++ Liya Tsegaye/alph square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+  int size = 5;
+  char c;
+  for (int i = 0; i < size; i++) {
+    
+    for (int j = 0; j < size; j++) {
+    
+    } for (int k=97; k<= 101; k++){
+    
+        cout << char(k);
+    }
+    cout << "\n";
+  }
+  return 0;
+  }
