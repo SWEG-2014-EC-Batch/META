007a0e9d270fd9b3d88d36e47e79ce4cdc16bb4f
Kaleab-Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2023-03-16 22:38:19 +0300

    Add files via upload

---
+++ printPattern5x.cxx
@@ -0,0 +1,31 @@
+//printPattern5x.cpp
+#include <iostream>
+using namespace std;
+int
+main ()
+{
+  int  n = 1, m = 0, g,f=7,h=8;
+
+  for (int size = 1; size <= f; size++)
+    {
+      for (int size = 1; size <= 1; size++)
+  {
+    cout << n;
+  } g = n + 1;
+      for (int size = 1; size <= m; size++)
+  {
+    cout <<" "<<g++<<"";
+  } m++;
+      cout << endl;
+     
+    }
+     for(int size=1;size<=8;size++)
+     {
+         cout<<h--<<" ";
+        
+     }cout<<endl;
+  
+    cout<<"*****This is the last line*****";
+
+  return 0;
+}
