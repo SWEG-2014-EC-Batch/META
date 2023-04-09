16345447214d307a48b1b7ca7a01fb08be581383
Jonathan Leoul <125746493+Jonathan-Le7@users.noreply.github.com>
2023-04-09 21:16:08 +0300

    Create AreaofCircle.cpp

---
+++ Jonathan Leoul/Class Excercises/AreaofCircle.cpp
@@ -0,0 +1,18 @@
+// Finding the area of a circle
+#include <iostream>
+using namespace std;
+#define PI 3
+
+int main () {
+int radius, area;
+
+cout <<"Enter Radius of cirlce/n";
+cin>>radius;
+//Area of Cirlce= PI x Radius x Radius
+area= PI*radius*radius;
+cout<<area;
+return 0;
+
+
+
+}
