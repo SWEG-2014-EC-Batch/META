63109ca10fb9df2707c90470953bfe7d4e69dd0c
Jonathan Leoul <125746493+Jonathan-Le7@users.noreply.github.com>
2023-06-11 13:24:10 +0300

    Add files via upload

---
+++ FOPII/homework/NumberMultiplier.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+double getProduct (int firstNum, double secondNum);
+int main () {
+    int firstNum; double secondNum;
+      double product= getProduct(firstNum,secondNum);
+      cout<<"Multiplicative result of these two numbers is :"<<product;
+    
+      return 0;
+}
+double getProduct (int firstNum, double secondNum){
+    cout<<" Insert 2 integers respectively (One normal and one double integer) "<<endl;
+    cin>>firstNum>>secondNum;
+
+    return firstNum*secondNum;
+
+}
