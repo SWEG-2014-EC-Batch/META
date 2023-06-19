47ef0df655485bea371f58f4439d44f6ca0421b5
Kaleab-Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2023-06-19 13:54:56 +0300

    Add files via upload

---
+++ FoP-II/Kaleab Hailemeskel/Classwork/mult2nums.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "newfile.h"
+using namespace std;
+
+float mult(float num1, float num2);
+
+int main()
+{   
+    int num1, num2;
+    
+    cout<<"Enter two numbers to operate multiplication: ";
+    cin>>num1>>num2;
+    cout<<num1<<"*"<<num2<<" ="<<mult(num1, num2);
+}
+
