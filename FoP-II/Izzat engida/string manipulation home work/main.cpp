7244e6a59fbfcb600fb679d5afc5695cb1a272ec
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2023-06-24 09:19:22 +0300

    main.cpp

---
+++ FoP-II/Izzat engida/string manipulation home work/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"lower.h"
+#include"upper.h"
+#include"rever.h"
+#include"compare.h"
+#include"length.h"
+#include"input.h"
+int main(){
+char*ptr;
+int size;
+input(ptr,size);
+return 0; 
+}
