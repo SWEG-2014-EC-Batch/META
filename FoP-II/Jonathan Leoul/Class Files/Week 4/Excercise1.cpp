7e777f9b709bf8fa45939617245ad67ae6818f20
Jonathan Leoul <125746493+Jonathan-Le7@users.noreply.github.com>
2023-06-27 12:01:06 +0300

    Create Excercise1.cpp

---
+++ FoP-II/Jonathan Leoul/Class Files/Week 4/Excercise1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+struct student {
+    long id;
+    char name[15];
+    int testmark;
+    int finalmark;
+};
+int N;
+cout<<"Insert the number of students to store informations: ";
+cin>>N;
+
+student stud[100];
+
+for( int i=0;i<N;i++){
+    cout<<endl<<" Number "<<i<<endl;
+cout<<"Insert Student ID number: ";
+cin>>stud[N].id;
+cout<<endl<<"Input name of the student: ";
+cin.ignore();
+cin.getline(stud[N].name,13);
+cout<<endl<<"Input Test mark: ";
+cin>>stud[N].testmark;
+cout<<endl<<"Input Final exam mark: ";
+cin>>stud[N].finalmark;
+cout<<endl<<"Name : ";
+cout<<stud[N].name;
+
+cout<<endl<<"ID : "<<stud[N].id<<endl;
+cout<<"Total mark = "<<stud[N].finalmark+stud[N].testmark;
+}
+}
